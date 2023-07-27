142e168ff78286dfe371f62805e06e80edb9e435
AllanLobato <allandonizetti@gmail.com>
2023-07-27 18:48:57 -0300

    feat: Add fonts, configs and routs

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
-import { Home } from './src/screens/Home';
+import { Home } from "@screens/Home";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/theme";
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_700Bold,
+} from "@expo-google-fonts/nunito";
 
 export default function App() {
+  //fontsLoaded recebe um valor boolean
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+      <StatusBar barStyle="light-content" translucent />
+    </ThemeProvider>
   );
 }
-
